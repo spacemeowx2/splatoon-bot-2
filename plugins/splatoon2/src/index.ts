014a06065017bd6608bd0c13b731e7e6f1dc024b
spacemeowx2 <spacemeowx2@gmail.com>
2022-08-06 16:13:22 +0800

    feat: hello world

--- plugins/splatoon2/src/index.ts
+++ plugins/splatoon2/src/index.ts
@@ -1,7 +1,13 @@
-import { Context } from 'koishi'
+import { Argv, Context } from 'koishi'
 
 export const name = 'splatoon2'
 
+async function stageHandler(argv: Argv) {
+  return 'hello world!'
+}
+
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('图', '获取当前或未来的地图')
+    .action(stageHandler)
+
 }
